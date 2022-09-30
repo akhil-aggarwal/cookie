f5f0833cbaac168d60a90a4fb04cff45cde69616
Akhil Aggarwal <58690693+akhil-aggarwal@users.noreply.github.com>
2022-09-30 16:04:17 +0530

    Updated script.js to V1.2
    Updated the code of cookies in script.js file.

--- script.js
+++ script.js
@@ -1,64 +1,45 @@
-console.log('Hey Script is working');
-const data = [ ];
+const _printData = [];
 
 function cookieFunction()
-{   
-    data[0] = document.getElementById('fNameInput').value;
-    data[1] = document.getElementById('lNameInput').value;
-    data[2] = document.getElementById('emailInput').value;
-    data[3] = document.getElementById('ageInput').value;
+{
+    Cookies.set("fName",document.getElementById("fNameInput").value);
+    Cookies.set("lName",document.getElementById("lNameInput").value);
+    Cookies.set("email",document.getElementById("emailInput").value);
+    Cookies.set("age",document.getElementById("ageInput").value);
+    setValues();
+}
 
-    return data;
+function setValues()
+{
+    _printData[0] = Cookies.get("fName");
+    _printData[1] = Cookies.get("lName");
+    _printData[2] = Cookies.get("email");
+    _printData[3] = Cookies.get("age");
+
+    printValues(_printData);
     
 }
 
-cookieFunction();
-
-Cookies.set('FName', data[0], {
-    expires: 10
-})
-Cookies.set('LName', data[1], {
-    expires: 10
-})
-Cookies.set('Email', data[2], {
-    expires: 10
-})
-Cookies.set('Age', data[3], {
-    expires: 10
-})
-
-var cFName = Cookies.get('FName');
-var cLName = Cookies.get('LName');
-var cEmail = Cookies.get('Email');
-var cAge = Cookies.get('Age');
-
-if ( Cookies.get('FName') != null && data[0] != undefined ) {
-
-    document.getElementById('startStr').innerHTML = "Hey " + cFName + ", Have a Look at Cookies!";
-    document.getElementById('resultName').innerHTML = cFName+ ',';
-    document.getElementById('btnFirstName').innerHTML = cFName;
-    document.getElementById('btnLastName'). innerHTML = cLName;
-    document.getElementById('btnEmail').innerHTML = cEmail;
-
-    if(cAge < 18)
-    {   
-        var remYears = 18 - cAge;
-   
-        document.getElementById('adultMessage').innerHTML = "You are not 18. Please wait for "+remYears+" years, <br> to make your Pan & Voter Card."
-        document.getElementById('adultSection').style.visibility = "hidden";
-   
+function printValues(_printData)
+{   
+    if (_printData[0] != undefined && _printData[0] != null) {
+        document.getElementById('startStr').innerHTML = "Hey " + _printData[0] + ", Have a Look at Cookies!";
+        document.getElementById('resultName').innerHTML = _printData[0] + ',';
+        document.getElementById('btnFirstName').innerHTML = _printData[0];
+        document.getElementById('btnLastName').innerHTML = _printData[1];
+        document.getElementById('btnEmail').innerHTML = _printData[2];
+    
+        if (_printData[3] < 18) {
+            var remYears = 18 - _printData[3];
+    
+            document.getElementById('adultMessage').innerHTML = "You are not 18. Please wait for " + remYears + " years, <br> to make your Pan & Voter Card."
+            document.getElementById('adultSection').style.visibility = "hidden";
+    
+        }   
     }
-
- }
-
- else
- {  
-     document.getElementById("textFormFilled").innerHTML = "You will get your cookie results here.<br> Please fill form to proceed."
-     document.getElementById("formFilled").style.visibility = "hidden";
- }
-
-
-
-
-
-
+    else
+    {
+        document.getElementById("textFormFilled").innerHTML = "You will get your cookie results here.<br> Please fill form to proceed."
+        document.getElementById("formFilled").style.visibility = "hidden";
+    }
+}
